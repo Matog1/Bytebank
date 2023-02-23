5ac1fcba446fa33e8096978e1701174b789afd03
Matog1 <84256186+Matog1@users.noreply.github.com>
2023-02-23 09:28:19 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+    namespace ByteBank
+{
+
+    class program
+    {
+
+        static void Main(string[] args)
+        {
+
+            int numero_agencia;
+            string conta;
+            string titular;
+            double saldo;
+
+
+
+        }
+    }
+}
